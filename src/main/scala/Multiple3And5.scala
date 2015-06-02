9022c5ce9f54d93f64023896e31bb01fde71135b
chandu kavar <cckavar@ymail.com>
2015-06-02 15:38:02 +0530

    Chandu Multiple3Or5 implemented

--- src/main/scala/Multiple3And5.scala
+++ src/main/scala/Multiple3And5.scala
@@ -1,10 +1,19 @@
 /**
  * Created by chanduk on 01/06/15.
  */
-class Multiple3And5 {
+object Multiple3And5{
 
-   def main (args: Array[String]) {
-
+  def main (args: Array[String]) {
+    multiplication35(1000)
   }
-  def multiplication35()
+  def multiplication35(num: Int)={
+    var sum =0
+    for(a <- 1 to num-1)
+    if(moduloBy3Or5(a)) sum = sum + a
+    print(sum)
+  }
+  def moduloBy3Or5(x: Int):Boolean={
+    if(x%3==0 || x%5==0) true
+    else false
+  }
 }
