5552d39b7db98c18edbce55d1ab53f80031ce303
chandu kavar <cckavar@ymail.com>
2015-06-02 16:03:11 +0530

    chandu without mutating version of Multiple3And5

--- src/main/scala/Multiple3And5.scala
+++ src/main/scala/Multiple3And5.scala
@@ -4,7 +4,8 @@
 object Multiple3And5{
 
   def main (args: Array[String]) {
-    multiplication35(1000)
+    //multiplication35(1000)
+    print(multi35(1000))
   }
   def multiplication35(num: Int)={
     var sum =0
@@ -16,4 +17,13 @@ object Multiple3And5{
     if(x%3==0 || x%5==0) true
     else false
   }
+//withoud mutating the variable
+  def multi35(num: Int)={
+    def multi(acc: Int, out: Int):Int=
+      if(acc>=num) out
+      else if(moduloBy3Or5(acc)) multi(acc+1,acc+out)
+      else multi(acc+1,out)
+    multi(1,0)
+  }
 }
+
